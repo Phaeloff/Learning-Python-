e2fbb5935fc77c790404541ec8e0c86b8208615a
Phael <raphael123@outlook.com.br>
2026-02-18 16:58:01 -0300

    aula 28

---
+++ curso-sql/28_create.sql
@@ -0,0 +1,15 @@
+DROP TABLE IF EXISTS clientes_d28;
+
+CREATE TABLE IF NOT EXISTS cliente_d28(
+    idCliente varchar(250) PRIMARY KEY,
+    Qtdetransacoes INTERGER
+);
+
+INSERT INTO clientes_d28
+SELECT idCliente,
+    count(DISTINCT IdTransacao) AS Qtdetransacoes
+
+FROM transacoes
+WHERE julianday('now') - julianday(substr(DtCriacao, 1,10)) <= 28
+GROUP BY IdCliente
+;
---
+++ curso-sql/28_update.sql
@@ -0,0 +1,8 @@
+SELECT * from relatorio_diario;
+
+UPDATE relatorio_diario
+SET qtdTransacao = 100000
+WHERE dtDia >= '2025-08-25'
+;
+
+SELECT * FROM relatorio_diario;
