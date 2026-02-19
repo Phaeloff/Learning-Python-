5324f785bce04aab2fa35cd83d7df64a511ec512
Phael <raphael123@outlook.com.br>
2026-02-19 16:47:14 -0300

    projeto-tb_analisty

---
+++ ProjetoEti/eti_projeto.sql
@@ -0,0 +1,106 @@
+WITH tb_transacoes AS (
+    SELECT
+            IdTransacao,
+            idCliente,
+            qtdePontos,
+            datetime(substr(DtCriacao,1,19)) AS DtCriacao,
+            julianday('now') - julianday (substr(DtCriacao,1,10)) AS diffDate
+    FROM transacoes
+),
+
+tb_cliente AS (
+    SELECT idCliente,
+        datetime(substr(DtCriacao,1,19)) AS DtCriacao,
+        julianday('now') - julianday (substr(DtCriacao,1,10)) AS idadeBase
+        FROM clientes
+),
+
+tb_sumario_transacoes AS (
+    SELECT idCliente,
+        count(IdTransacao) as qtdeTransacoesVida,
+        count(CASE WHEN diffDate <= 56 THEN IdTransacao END) AS qtdeTransacoes56Dias,
+        count(CASE WHEN diffDate <= 28 THEN IdTransacao END) AS qtdeTransacoes28Dias,
+        count(CASE WHEN diffDate <= 14 THEN IdTransacao END) AS qtdeTransacoes14Dias,
+        count(CASE WHEN diffDate <= 7 THEN IdTransacao END) AS qtdeTransacoes7Dias,
+
+        sum(qtdePontos) AS saldoPontos,
+
+        min(diffDate) AS diasDesdeUltimaInteracao,
+
+        sum(CASE WHEN QtdePontos > 0 THEN qtdePontos ELSE 0 END) AS qtdePontosPosVida,
+        sum(CASE WHEN qtdePontos > 0 AND diffDate <= 56 THEN qtdePontos END) AS qtdePontosPos56Dias,
+        sum(CASE WHEN qtdePontos > 0 AND diffDate <= 28 THEN qtdePontos END) AS qtdePontosPos28Dias,
+        sum(CASE WHEN qtdePontos > 0 AND diffDate <= 14 THEN qtdePontos END) AS qtdePontosPos14Dias,
+        sum(CASE WHEN qtdePontos > 0 AND diffDate <= 7 THEN qtdePontos END) AS qtdePontosPos7Dias,
+
+        sum(CASE WHEN QtdePontos < 0 THEN qtdePontos ELSE 0 END) AS qtdePontosNegVida,
+        sum(CASE WHEN qtdePontos < 0 AND diffDate <= 56 THEN qtdePontos END) AS qtdePontosNeg56Dias,
+        sum(CASE WHEN qtdePontos < 0 AND diffDate <= 28 THEN qtdePontos END) AS qtdePontosNeg28Dias,
+        sum(CASE WHEN qtdePontos < 0 AND diffDate <= 14 THEN qtdePontos END) AS qtdePontosNeg14Dias,
+        sum(CASE WHEN qtdePontos < 0 AND diffDate <= 7 THEN qtdePontos END) AS qtdePontosNeg7Dias
+
+    FROM tb_transacoes
+    GROUP BY idCliente
+),
+
+tb_transacao_produto AS (
+
+    SELECT
+        t1.*,
+        t3.DescNomeProduto,
+        t3.DescCategoriaProduto
+        
+    FROM tb_transacoes AS t1
+
+    LEFT JOIN transacao_produto AS t2
+    ON t1.IdTransacao = t2.IdTransacao
+
+    LEFT join produtos AS t3
+    ON t2.IdProduto = t3.IdProduto
+
+),
+
+tb_cliente_produto AS(
+    SELECT idCliente,
+        DescNomeProduto,
+        count(*) AS qtdeVida,
+        count(CASE WHEN diffDate <= 56 THEN IdTransacao END) AS qtde56,
+        count(CASE WHEN diffDate <= 28 THEN IdTransacao END) AS qtde28,
+        count(CASE WHEN diffDate <= 14 THEN IdTransacao END) AS qtde14,
+        count(CASE WHEN diffDate <= 7 THEN IdTransacao END) AS qtde7
+
+    FROM tb_transacao_produto
+    GROUP BY idCliente, DescNomeProduto
+),
+
+tb_cliente_produto_rn AS (
+    SELECT *,
+        row_number() OVER (PARTITION BY idCliente ORDER BY qtdeVida DESC) AS rankingVida,
+        row_number() OVER (PARTITION BY idCliente ORDER BY qtde56 DESC) AS ranking56,
+        row_number() OVER (PARTITION BY idCliente ORDER BY qtde28 DESC) AS ranking28,
+        row_number() OVER (PARTITION BY idCliente ORDER BY qtde14 DESC) AS ranking14,
+        row_number() OVER (PARTITION BY idCliente ORDER BY qtde7 DESC) AS ranking7
+
+    FROM tb_cliente_produto
+),
+
+tb_join AS (
+    SELECT
+            t1.*,
+            t2.idadeBase,
+            t3.DescNomeProduto AS produtoMaisConsumidoVida
+
+    FROM tb_sumario_transacoes AS t1
+
+    LEFT JOIN tb_cliente AS t2
+    ON t1.idCliente = t2.idCliente
+
+    LEFT JOIN tb_cliente_produto_rn AS t3
+    ON t1.idCliente = t3.idCliente AND t3.rankingVida = 1
+
+
+)
+
+
+SELECT * FROM tb_join
+ORDER BY idCliente
