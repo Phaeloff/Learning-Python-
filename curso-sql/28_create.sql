58bb541357765915304c39675c88b66216aeb956
Phael <raphael123@outlook.com.br>
2026-02-19 15:19:04 -0300

    28-create

--- curso-sql/28_create.sql
+++ curso-sql/28_create.sql
@@ -1,6 +1,6 @@
 DROP TABLE IF EXISTS clientes_d28;
 
-CREATE TABLE IF NOT EXISTS cliente_d28(
+CREATE TABLE IF NOT EXISTS cliente_d28 (
     idCliente varchar(250) PRIMARY KEY,
     Qtdetransacoes INTERGER
 );
@@ -13,3 +13,5 @@ FROM transacoes
 WHERE julianday('now') - julianday(substr(DtCriacao, 1,10)) <= 28
 GROUP BY IdCliente
 ;
+
+SELECT * FROM clientes_d28;
