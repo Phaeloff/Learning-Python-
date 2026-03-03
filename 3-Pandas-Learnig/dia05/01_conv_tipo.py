63f3e0538f995332734a7596d8a9c5473d49c031
Phael <raphael123@outlook.com.br>
2026-03-03 14:37:54 -0300

    ep-08

---
+++ 3-Pandas-Learnig/dia05/01_conv_tipo.py
@@ -0,0 +1,34 @@
+# %%
+
+import pandas as pd
+
+# %%
+
+df = pd.read_csv("../data/clientes.csv")
+
+df
+
+# %% - inteiro
+
+df["qtdePontos"].astype(int)
+
+#%% - float
+
+df["qtdePontos"].astype(float)
+
+# %% - string
+df["qtdePontos"].astype(str)
+
+# %% - converter data
+replace = {
+    "0000-00-00 00:00:00.000": "2024-02-01 09:00:00.000",
+    "NaN" : "2024-02-01 09:00:00.000"
+    }
+
+df["dtCriacao"] = pd.to_datetime(df["dtCriacao"].replace{replace})
+# %%
+
+df["dtCriacao"].dt.year()
+
+
+# %%
